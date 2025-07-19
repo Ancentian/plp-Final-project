99d3d472f62402c64e3eac1f2b8ee7028115369e
unknown <ancentmbithi8@gmail.com>
2025-07-19 18:35:22 +0300

    URL Updates

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express()
 const bodyParser = require("body-parser");
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true
   }));
 
